a5fcb2d5b6eb7b0b28fa2345ed388b764805d1d3
Prashant428 <prashant428@gmail.com>
2021-12-13 23:23:19 -0500

    env variable

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 module.exports = {
     //"URI": "mongodb://localhost/survey",
-    "secretKey":"<DEFINE_YOUR_SECRET_HERE>",
-    "URI":"mongodb+srv://manav:chT0XlUgGIm0CWgI@mongodbserver.yw8mu.mongodb.net/Surveys?retryWrites=true&w=majority"
+    "secretKey":process.env.SECRETKEY,
+    //"<DEFINE_YOUR_SECRET_HERE>",
+    "URI": process.env.ATLASDB
+    //"mongodb+srv://manav:chT0XlUgGIm0CWgI@mongodbserver.yw8mu.mongodb.net/Surveys?retryWrites=true&w=majority"
 }
